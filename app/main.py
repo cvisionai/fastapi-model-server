0087c4f0f28a7bb8fb6bb5b47fafff798ddbbf07
Benjamin Woodward <benjamin.woodward@cvisionai.com>
2023-04-25 21:14:19 -0400

    update python, include SAM endpoint (deprecated)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 # import the necessary packages
-from fastapi import FastAPI, File, UploadFile, Form
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile, Form, Body
+from pydantic import BaseModel, parse_obj_as
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 from PIL import Image
+from typing import List, Dict
 import numpy as np
 import os
 import app.helpers as helpers
@@ -16,6 +17,7 @@ import time
 import json
 import io
 import logging
+import cv2
 
 logging.basicConfig(
     handlers=[logging.StreamHandler()],
@@ -51,6 +53,12 @@ db.ping()
 
 print(f"connected to redis: {os.getenv('REDIS_HOST')}") 
 
+class SamPrompt(BaseModel):
+    x1 : int
+    x2 : int
+    w : int = -1
+    h : int = -1
+
 def prepare_image(image, target):
     # if the image mode is not RGB, convert it
     if image.mode != "RGB":
@@ -68,6 +76,71 @@ def prepare_image(image, target):
 @app.get("/")
 async def homepage():
     return FileResponse('/static-files/index.html')
+
+@app.post("/sam/")
+def predict_sam(data: str = Form(...), file: UploadFile = File(...)):
+    logger.info(data)
+    
+    #parsed_data = parse_obj_as(List[SamPrompt], data)
+    return_data = {"success": False}
+    logger.info(f"Prompts: {data}")
+    data = json.loads(data)
+    contents = file.file.read()
+    np_array = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(np_array,cv2.IMREAD_COLOR)
+
+    #image = Image.open(file.file)
+    height, width, channels = image.shape
+    # if the image mode is not RGB, convert it
+    if channels == 1:
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+    elif channels == 4:
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
+    else:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    #image = np.array(image).astype('float32')
+    #image = np.array(image).astype('uint8')
+    # convert to BGR, because that's what the model expects
+    #image = image[:, :, ::-1].copy()
+    image = np.expand_dims(image, axis=0)
+    logger.info(f"Image original dimesions: {width}x{height}")
+    # ensure our NumPy array is C-contiguous as well,
+    # otherwise we won't be able to serialize it
+    image = image.copy(order="C")
+
+    k = str(uuid.uuid4())
+    image = helpers.base64_encode_image(image)
+    d = {"id": k, "image": image, "height": height, "width": width, "prompts": data}
+    db.rpush("image_queue_sam", json.dumps(d))
+
+    while True:
+        # attempt to grab the output predictions
+        output = db.get(k)
+
+        # check to see if our model has classified the input
+        # image
+        # print(f"  - output: {output}")
+        if output is not None:
+            # add the output predictions to our data
+            # dictionary so we can return it to the client
+            output = output.decode("utf-8")
+            return_data["predictions"] = json.loads(output)
+
+            # delete the result from the database and break
+            # from the polling loop
+            db.delete(k)
+            break
+
+        # sleep for a small amount to give the model a chance
+        # to classify the input image
+        time.sleep(float(os.getenv("CLIENT_SLEEP")))
+
+    # indicate that the request was a success
+    return_data["success"] = True
+    return_data = jsonable_encoder(return_data)
+    # return the data dictionary as a JSON response
+    return JSONResponse(content=return_data)
 
 @app.post("/predictor/")
 def predict(model_type: str = Form(...), file: UploadFile = File(...)):
--- script/simple_request.py
+++ script/simple_request.py
@@ -2,23 +2,25 @@
 # python simple_request.py
 
 # import the necessary packages
-import requests
+import requests, time
 
 # initialize the Keras REST API endpoint URL along with the input
 # image path
-MODEL_REST_API_URL = "https://your_domain_url:port/predictor/"
+MODEL_REST_API_URL = "https://adamant.tator.io:8082/predictor/"
+#MODEL_REST_API_URL = "https://your_domain_url:port/predictor/"
 IMAGE_PATH = "../images/00_01_13_13.png"
 
 # load the input image and construct the payload for the request
 image = open(IMAGE_PATH, "rb").read()
 payload = {
     "file" : ("file", image, "image/png"),
-    "model_type" : (None, "image_queue_yolov5")
+    "model_type" : (None, "image_queue_detectron2")
 }
-
+start_time = time.time()
 # submit the request
 r = requests.post(MODEL_REST_API_URL, files=payload).json()
 # ensure the request was sucessful
+print(f"Request took {time.time() - start_time}")
 if r["success"]:
 	# loop over the predictions and display them
 	for (i, result) in enumerate(r["predictions"]):
@@ -27,3 +29,19 @@ if r["success"]:
 # otherwise, the request failed
 else:
 	print("Request failed")
+
+
+MODEL_REST_API_URL = "https://adamant.tator.io:8082/sam/"
+payload = {
+	"file" : ("file", image, "image/png"),
+	"prompt_list" : [
+	{"x" : 252,
+     "y" : 782}
+]
+}
+data = [
+	{"x" : 252,
+     "y" : 782}
+]
+
+r = requests.post(MODEL_REST_API_URL, files=payload).json()
